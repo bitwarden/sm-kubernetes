3b8183983eb952e1867ef4a7d19fa838f7afc129
Joel Beskow <joel.beskow@quisitive.com>
2024-02-28 17:25:49 -0600

    Finishing up the Dockerfile, fixing the role assignments to allow the operator to manage secrets at the cluster level, and starting on the Helm chart creation.

--- src/sm-operator/internal/controller/bitwardensecret_controller.go
+++ src/sm-operator/internal/controller/bitwardensecret_controller.go
@@ -52,6 +52,8 @@ type BitwardenSecretReconciler struct {
 //+kubebuilder:rbac:groups=k8s.bitwarden.com,resources=bitwardensecrets,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=k8s.bitwarden.com,resources=bitwardensecrets/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=k8s.bitwarden.com,resources=bitwardensecrets/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=secrets/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
